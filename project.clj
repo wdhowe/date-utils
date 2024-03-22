edda82aed5546b3ac54eeec9a7f90972c4ad6eab
Bill Howe <wdhowe@protonmail.com>
2024-03-21 23:11:13 -0500

    Convert project management to mainly lein. Allow a minimal deps.edn for git repo deps referencing.

---
+++ project.clj
@@ -0,0 +1,17 @@
+(defproject date-utils "0.1.0"
+
+  ;;; Project Metadata
+  :description "A Clojure library for getting date based information."
+  :url "https://github.com/wdhowe/date-utils"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+
+  ;;; Dependencies, Plugins
+  :dependencies [[org.clojure/clojure "1.11.2"]
+                 [clojure.java-time/clojure.java-time "1.4.2"]]
+
+  ;;; Profiles
+  :profiles {:uberjar {:aot :all}}
+
+  ;;; Running Project Code
+  :repl-options {:init-ns date-utils.core})
